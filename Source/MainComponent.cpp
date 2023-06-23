c8ef4938269a678d3b875e3328f4fef1724a73a9
LeeTaeHwa <driversivese@gmail.com>
2023-06-23 16:54:03 +0900

    component update

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -17,6 +17,7 @@ MainComponent::MainComponent()
 
 MainComponent::~MainComponent()
 {
+    playerWindow.release();
 }
 
 //==============================================================================
@@ -35,6 +36,27 @@ void MainComponent::resized() {
 }
 
 auto MainComponent::openMusicPlayer() -> void {
+    juce::AlertWindow::showAsync(juce::MessageBoxOptions()
+        .withIconType(juce::MessageBoxIconType::InfoIcon)
+        .withTitle("Permissions warning")
+        .withMessage("External storage access permission not granted, some files"
+            " may be inaccessible.")
+        .withButton("OK")
+        .withButton("More.."),
+        juce::ModalCallbackFunction::create([](int index) {
+            auto msg = std::string();
+
+            if (index == 1) msg = "You pressed Ok";
+            else msg = "You pressed More..";
+
+            juce::AlertWindow::showAsync(juce::MessageBoxOptions()
+                .withIconType(juce::MessageBoxIconType::InfoIcon)
+                .withTitle("Result")
+                .withMessage(msg),
+                nullptr);
+        })
+    );
+
     playerWindow.reset(new akasha::PlayerWindow(akasha::musicPlayerName));
     playerWindow->setBounds(200, 200, 300, 300);
     playerWindow->setVisible(true);
